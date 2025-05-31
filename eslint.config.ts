3f3c73d1f5d07a540f8b6e6841e0eed895635dd5
hsiao-i <eva090029@gmail.com>
2025-05-31 11:38:42 +0800

    create project

---
+++ eslint.config.ts
@@ -0,0 +1,48 @@
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+  {
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [['@', './src']],
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+        },
+      },
+    },
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  pluginVue.configs['flat/recommended'],
+  // pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  // 這裏已經處理了 ESLint 與 Prettier 的規則衝突
+  skipFormatting,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      // 允許三元運算子、及 a && b、 || 等運算子
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+      'prettier/prettier': 'warn',
+    },
+  },
+)
