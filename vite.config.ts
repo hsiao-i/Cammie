eaf27189429e1e6db03d85fa407f6f6e8666f66e
hsiao-i <eva090029@gmail.com>
2025-06-01 23:56:12 +0800

    feat: Add and revise config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'node:path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'CammieLibrary',
+      formats: ['es', 'cjs', 'iife'],
+      // the proper extensions will be added
+      fileName: 'CammieLibrary',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
