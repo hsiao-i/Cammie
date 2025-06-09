d9b7163aaf29541a57f5c4c13930dda685c45b84
hsiao-i <eva090029@gmail.com>
2025-06-09 23:21:01 +0800

    feat: Add button component

---
+++ src/components/button/CmButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { Button } from '@/types/button';
+import { computed } from 'vue';
+
+const props = withDefaults(defineProps<Button>(), {
+  type: 'button',
+  size: 'normal',
+  color: 'primary',
+});
+
+const buttonClass = computed(() => {
+  const size = props.size === 'normal' ? '' : 'btn-sm';
+
+  const colorMapping = {
+    primary: 'btn-primary-500',
+    secondary: 'btn-secondary',
+    success: 'btn-success-500',
+    warning: 'btn-warning-500',
+    danger: 'btn-danger-500',
+    info: 'btn-info-500',
+  };
+
+  const color = colorMapping[props.color];
+
+  return [size, color];
+});
+</script>
+<template>
+  <button :class="buttonClass" class="btn" :type="props.type">
+    <slot />
+  </button>
+</template>
---
+++ src/types/button.ts
@@ -0,0 +1,5 @@
+export interface Button {
+  type?: 'button' | 'submit' | 'reset';
+  size?: 'normal' | 'small';
+  color?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info';
+}
