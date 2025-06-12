e17d0d526200cf8e863571035085946d94aff600
hsiao-i <eva090029@gmail.com>
2025-06-12 22:47:11 +0800

    feat: Export Link component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import '@/assets/css/tailwindStyle.css';
 
 export { default as CmButton } from './components/button/CmButton.vue';
+export { default as CmLink } from './components/link/CmLink.vue';
